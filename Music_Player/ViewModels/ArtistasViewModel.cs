f557e45a6541ec3519f4e93b6995fd74ac3e3374
Carlos Daniel Maldonado Muñoz <201G0266@rcarbonifera.tecnm.mx>
2023-06-10 15:36:13 -0600

    l

--- Music_Player/ViewModels/ArtistasViewModel.cs
+++ Music_Player/ViewModels/ArtistasViewModel.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore.Update.Internal;
 using Music_Player.Catalogos;
 using Music_Player.Models;
 using Music_Player.Operaciones;
+using Music_Player.Views.Enum_CambiarVista;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -21,22 +22,22 @@ namespace Music_Player.ViewModels
          public ICommand AgregarArtistaCommand { get; set; }
         public ICommand EditarArtistaCommand { get; set; }
         public ICommand EliminarArtistaCommand { get; set; }
-
+        public VistaArtista Vista { get; set; } = VistaArtista.VerArtistas;
         Artista artista;
         public ArtistasViewModel()
         {
             //ActualizarListaArtistas();
         }
-        //public void ActualizarListaArtistas()
-        //{
-        //    ListaArtistas.Clear();
-        //    foreach (var item in catalogo_Art.GetArtistas())
-        //    {
-        //        ListaArtistas.Add(item);
-        //    }
-        //    Actualizar();
-            
-        //}
+        public void ActualizarListaArtistas()
+        {
+            ListaArtistas.Clear();
+            foreach (var item in catalogo_Art.GetArtistas())
+            {
+                ListaArtistas.Add(item);
+            }
+            Actualizar();
+
+        }
         public void AgregarArtista()
         {
 
---
+++ Music_Player/Views/Enum_CambiarVista/EnumArtistaVistas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Player.Views.Enum_CambiarVista
+{
+    public enum VistaArtista
+    {
+        VerArtistas,
+        VerCancionesPorArtista,
+        AgregarArtista,
+        EditarArtista,
+        EliminarArtista,
+        VerArtistasPorGenero
+    }
+
+}
